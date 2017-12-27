e34d5d47fae27d7f780d7294b147e2469fcfe317
tatsuya <tatsuya_hirota@kronos.jp>
2017-12-27 18:26:18 +0900

    update

--- src/twolayernet.py
+++ src/twolayernet.py
@@ -28,6 +28,7 @@ def cross_entropy_error(y, t):
   batch_size = y.shape[0]
   return -np.sum(t * np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
+# 勾配の計算
 def numerical_gradient(f, x):
   h = 1e-4 # 0.0001
   grad = np.zeros_like(x)
@@ -37,13 +38,14 @@ def numerical_gradient(f, x):
     tmp_val = x[idx]
     x[idx] = float(tmp_val) + h
     fxh1 = f(x) # f(x+h)
-    x[idx] = tmp_val - h 
+    x[idx] = tmp_val - h
     fxh2 = f(x) # f(x-h)
     grad[idx] = (fxh1 - fxh2) / (2*h)
     x[idx] = tmp_val # 値を元に戻す
-    it.iternext()   
+    it.iternext()
   return grad
 
+# ニューラルネットワーク
 class TwoLayerNet:
   # input_size: 入力ニューロン数, hidden_size: 隠れ層のニューロン数, output_size: 出力層のニューロン数
   def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
@@ -73,7 +75,7 @@ class TwoLayerNet:
     y = self.predict(x)
     return cross_entropy_error(y, t) # 交差エントロピー誤差
 
-  # 認識制度を求める
+  # 認識精度を求める
   # x: 入力データ(画像データ), t: 教師データ(正解ラベル)
   def accuracy(self, x, t):
     y = self.predict(x)
@@ -94,52 +96,64 @@ class TwoLayerNet:
     grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
     return grads
 
-(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
+# ミニバッチ学習
+def mini_batch_study():
+  (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+  train_loss_list = []
 
-# ハイパーパラメータ
-iters_num = 10000
-train_size = x_train.shape[0]
-batch_size = 100
-learning_rate = 0.1
+  train_acc_list = []
+  test_acc_list = []
 
-iter_per_epoch = max(train_size / batch_size, 1)
+  # ハイパーパラメータ
+  iters_num = 1000 # 試行回数
+  train_size = x_train.shape[0] # 学習データの画像の枚数
+  batch_size = 20 # ミニバッチを作る数
+  learning_rate = 0.1 # 学習率
 
-network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+  # エポックの計算
+  iter_per_epoch = max(iters_num / batch_size, 1)
 
-for i in range(iters_num):
-  # ミニバッチの取得
-  batch_mask = np.random.choice(train_size, batch_size)
-  x_batch = x_train[batch_mask]
-  t_batch = t_train[batch_mask]
+  # ニューラルネットワークのオブジェクトの作成
+  network = TwoLayerNet(input_size=784, hidden_size=5, output_size=10)
 
-  # 勾配の計算
-  grad = network.numerical_gradient(x_batch, t_batch)
+  # 試行回数ループ
+  for i in range(iters_num):
+    # ミニバッチの取得
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
 
-  # パラメータの更新
-  for key in ('W1', 'b1', 'W2', 'b2'):
-    network.params[key] -= learning_rate * grad[key]
+    # 勾配の計算
+    grad = network.numerical_gradient(x_batch, t_batch)
 
-  # 学習経過の記録
-  loss = network.loss(x_batch, t_batch)
-  train_loss_list.append(loss)
+    # パラメータの更新
+    for key in ('W1', 'b1', 'W2', 'b2'):
+      network.params[key] -= learning_rate * grad[key]
 
-  if i % iter_per_epoch == 0:
-    train_acc = network.accuracy(x_train, t_train)
-    test_acc = network.accuracy(x_test, t_test)
-    train_acc_list.append(train_acc)
-    test_acc_list.append(test_acc)
-    print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+    # 学習経過の記録
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    # エポック数ループ
+    if i % iter_per_epoch == 0:
+      # 認識精度の取得
+      train_acc = network.accuracy(x_train, t_train)
+      test_acc = network.accuracy(x_test, t_test)
+      train_acc_list.append(train_acc)
+      test_acc_list.append(test_acc)
+      print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+  return train_acc_list
 
 # グラフの描画
-markers = {'train': 'o', 'test': 's'}
-x = np.arange(len(train_acc_list))
-plt.plot(x, train_acc_list, label='train acc')
-plt.plot(x, test_acc_list, label='test acc', linestyle='--')
-plt.xlabel("epochs")
-plt.ylabel("accuracy")
-plt.ylim(0, 1.0)
-plt.legend(loc='lower right')
-plt.show()
+def show_plot(train_acc_list):
+  markers = {'train': 'o', 'test': 's'}
+  x = np.arange(len(train_acc_list))
+  plt.plot(x, train_acc_list, label='train acc')
+  # plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+  plt.xlabel("epochs")
+  plt.ylabel("accuracy")
+  plt.ylim(0, 1.0)
+  plt.legend(loc='lower right')
+  plt.show()
+
+show_plot(mini_batch_study())
